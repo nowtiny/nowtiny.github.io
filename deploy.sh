e0e6e767c3e7031acb10d1d0c0275550d3fa193c
stwind <stwindfy@gmail.com>
2017-07-14 22:55:46 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -e
+
 update() {
-    git pull origin master
+    git init
     git add -A
     git commit -m "Site updated at $(date +%Y-%m-%dT%H:%M:%S)"
+    git remote add origin git@github.com:nowtiny/nowtiny.github.io.git
+    git remote update
+    git merge origin/master --allow-unrelated-histories -X ours --no-edit
     git push origin master
 }
 
